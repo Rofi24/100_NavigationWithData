c293af45b5c16b317551a757fd9a5348d97b5818
Irawan rofi hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-11-14 18:44:34 +0700

    Membuat HalamanForm

---
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -0,0 +1,8 @@
+package com.example.esjumbo
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanForm(){}
